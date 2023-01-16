4573a1d2b89b2b2b3890e69147e502b11087b612
Glenn Larsen <50579947+glennlarsen@users.noreply.github.com>
2023-01-16 16:06:03 +0100

    maxx phone to 20

--- src/constants/validationRules.js
+++ src/constants/validationRules.js
@@ -1,3 +1,3 @@
 export const EMAIL_REGEX = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
-export const MAX_PHONE_NUMBERS = 14;
+export const MAX_PHONE_NUMBERS = 20;
 export const MIN_PHONE_NUMBERS = 4;
